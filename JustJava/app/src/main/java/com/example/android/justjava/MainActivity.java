507aeb71892e3ad4f48707e0aa8039931b579e0d
Peter Henniger <henniger.peter@gmail.com>
2017-03-12 10:30:30 +0100

    added pricetag

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String priceMessage = "Free";
+        String priceMessage = "Price $" + (quantity*5);
         displayMessage(priceMessage);
     }
 

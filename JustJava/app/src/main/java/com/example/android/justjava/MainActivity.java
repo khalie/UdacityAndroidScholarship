cd7bc1305895eb1c8adeba68a51c9f0385a09fe3
Peter Henniger <henniger.peter@gmail.com>
2017-03-12 15:09:53 +0100

    renamed method

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
@@ -57,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public void inc(View view) {
         quantity = quantity + 1;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     /**
@@ -65,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public void dec(View view) {
         quantity = quantity - 1;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
 }
